8429660791c95f76e6bfb6da0d93cefa11f561e3
Kin Lo <kinabcd@gmail.com>
2024-08-18 16:15:44 +0800

    add AssertSliceEquals

--- testing/assert.go
+++ testing/assert.go
@@ -1,13 +1,19 @@
 package testing
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func AssertNoError(t *testing.T, o error) {
 	Assert(t, o == nil, "expect nil but %v", o)
 }
 
-func AssertEquals(t *testing.T, a, b any) {
+func AssertEquals[E comparable](t *testing.T, a, b E) {
 	Assert(t, a == b, "expect %v but %v", a, b)
+}
+func AssertSliceEquals[E comparable, S ~[]E](t *testing.T, a, b S) {
+	Assert(t, slices.Equal(a, b), "expect %v but %v", a, b)
 }
 func Assert(t *testing.T, ok bool, message string, args ...any) {
 	if !ok {
