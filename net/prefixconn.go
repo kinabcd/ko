4fee6230c70805f5311ac7f70f09dae5872dbbc7
Kin Lo <kinabcd@gmail.com>
2024-09-02 23:02:21 +0800

    implement WriterTo and ReaderFrom for PrefixConn

--- net/prefixconn.go
+++ net/prefixconn.go
@@ -5,6 +5,12 @@ import (
 	"net"
 )
 
+var (
+	_ io.ReadWriteCloser = &PrefixConn{}
+	_ io.WriterTo        = &PrefixConn{}
+	_ io.ReaderFrom      = &PrefixConn{}
+)
+
 // PrefixConn will first read Prefix until EOF, and then read the content from Conn.
 type PrefixConn struct {
 	net.Conn
@@ -21,4 +27,22 @@ func (b *PrefixConn) Read(p []byte) (n int, err error) {
 		}
 	}
 	return b.Conn.Read(p)
+}
+
+func (b *PrefixConn) WriteTo(w io.Writer) (n int64, err error) {
+	var nn int64
+	if b.Prefix != nil {
+		nn, err = io.Copy(w, b.Prefix)
+		n += nn
+		if err != nil && err != io.EOF {
+			return
+		}
+	}
+	nn, err = io.Copy(w, b.Conn)
+	n += nn
+	return
+}
+
+func (b *PrefixConn) ReadFrom(r io.Reader) (n int64, err error) {
+	return io.Copy(b.Conn, r)
 }
