96c65b922c25e6643ec6030bceff98c6899f9ed3
Kin Lo <kinabcd@gmail.com>
2024-09-10 08:38:20 +0800

    Pair, Triple, DialFunc, DialContextFunc

---
+++ ko.go
@@ -0,0 +1,12 @@
+package ko
+
+type Pair[A any, B any] struct {
+	First  A
+	Second B
+}
+
+type Triple[A any, B any, C any] struct {
+	First  A
+	Second B
+	Third  C
+}
--- net/net.go
+++ net/net.go
@@ -16,6 +16,25 @@ type Dialer interface {
 	// Dial connects to the given address
 	Dial(network, addr string) (c net.Conn, err error)
 }
+
+type DialFunc func(network, address string) (net.Conn, error)
+
+func (f DialFunc) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return f(network, address)
+}
+func (f DialFunc) Dial(network, address string) (net.Conn, error) {
+	return f(network, address)
+}
+
+type DialContextFunc func(ctx context.Context, network, address string) (net.Conn, error)
+
+func (f DialContextFunc) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return f(ctx, network, address)
+}
+func (f DialContextFunc) Dial(network, address string) (net.Conn, error) {
+	return f(context.Background(), network, address)
+}
+
 type Listener net.Listener
 
 type Forwarder interface {
