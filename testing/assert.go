131c44f38972c839b661dd4c07ba8cdb775e69c0
Kin Lo <kinabcd@gmail.com>
2024-09-21 09:25:44 +0800

    print stack in assert

--- testing/assert.go
+++ testing/assert.go
@@ -1,22 +1,33 @@
 package testing
 
 import (
+	"fmt"
+	"runtime/debug"
 	"slices"
+	"strings"
 	"testing"
 )
 
 func AssertNoError(t *testing.T, o error) {
-	Assert(t, o == nil, "expect nil but %v", o)
+	assert(t, o == nil, "expect nil but %v", o)
 }
 
 func AssertEquals[E comparable](t *testing.T, a, b E) {
-	Assert(t, a == b, "expect %v but %v", a, b)
+	assert(t, a == b, "expect %v but %v", a, b)
 }
 func AssertSliceEquals[E comparable, S ~[]E](t *testing.T, a, b S) {
-	Assert(t, slices.Equal(a, b), "expect %v but %v", a, b)
+	assert(t, slices.Equal(a, b), "expect %v but %v", a, b)
 }
 func Assert(t *testing.T, ok bool, message string, args ...any) {
+	assert(t, ok, message, args...)
+}
+
+func assert(t *testing.T, ok bool, message string, args ...any) {
 	if !ok {
-		t.Fatalf(message, args...)
+		stack := string(debug.Stack())
+		sp := strings.Split(stack, "\n")
+		newStack := slices.Concat(sp[:1], sp[7:])
+		msg := fmt.Sprintf(message, args...)
+		t.Fatal(msg, "\n", strings.Join(newStack, "\n"))
 	}
 }
